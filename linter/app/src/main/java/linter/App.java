99755829608cdbf7425f3d399562ea8d7028393d
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-09-22 20:44:46 -0700

    finishes linter functionality

--- linter/app/src/main/java/linter/App.java
+++ linter/app/src/main/java/linter/App.java
@@ -14,18 +14,27 @@ public class App {
     }
 
     public static void main(String[] args) throws IOException {
+        jsLinter("./app/src/main/resources/gates.js");
+    }
+    //"./app/src/main/resources/gates.js"
+    public static String jsLinter(String pathToFile) {
         int lineCounter = 0;
-        Path jsPath = Paths.get("./app/src/main/resources/gates.js");
+        String finalString = "";
+        Path jsPath = Paths.get(pathToFile);
         Scanner scanner = null;
-        scanner = new Scanner(jsPath);
-
-        while(scanner.hasNextLine()) {
-            lineCounter++;
-            String currentLine = scanner.nextLine();
-            if(!(currentLine.endsWith(";") || currentLine.endsWith("{") ||currentLine.endsWith("}") || currentLine.isEmpty() || currentLine.contains("if") || currentLine.contains("else"))) {
-                System.out.println("Line " +lineCounter+ ": Missing semicolon.");
+        try{
+            scanner = new Scanner(jsPath);
+            while(scanner.hasNextLine()) {
+                lineCounter++;
+                String currentLine = scanner.nextLine();
+                if(!(currentLine.endsWith(";") || currentLine.endsWith("{") ||currentLine.endsWith("}") || currentLine.isEmpty() || currentLine.contains("if") || currentLine.contains("else") || currentLine.startsWith("//"))) {
+                    finalString += "Line " +lineCounter+ ": Missing semicolon.\n";
+                }
             }
+            System.out.println(finalString);
+        } catch (Exception e) {
+            System.out.println(e);
         }
-
+        return finalString;
     }
 }
